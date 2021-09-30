d8d48d02cad0a79760d890988529550617861787
taeyeon <taeyeon.hwang@cubewiz.co.kr>
2021-09-30 17:15:23 +0900

    🚀 router & vuex 추가한 컴포넌트 추가

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>About {{ name }}</h1>
+</template>
+
+<script lang='ts'>
+  import { computed } from 'vue'
+  import { useStore } from 'vuex'
+
+  const store = useStore()
+
+  const name = computed(()=> {
+    return store.state.user.name
+  })
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>HomePi</p>
+
+  <p>Name in store is: {{ name }} </p>
+
+  <input v-model='newName'/>
+
+  <button @click='saveName'>Submit</button>
+</template>
+
+<script setup>
+  import { useStore } from 'vuex'
+  import { computed, ref } from 'vue'
+  const store = useStore()
+  import { useRouter } from 'vue-router'
+  const router = useRouter()
+
+  const name = computed(()=> {
+    return store.state.user.name
+  })
+
+  const newName = ref('')
+  function saveName() {
+    store.dispatch('saveName',newName.value)
+    newName.value =''
+    router.push('/about')
+  }
+</script>
+
+<style scoped>
+
+</style>
