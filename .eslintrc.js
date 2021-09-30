ac15330ff7649d632139d8d61bc21eb59900795e
taeyeon <taeyeon.hwang@cubewiz.co.kr>
2021-09-30 17:12:35 +0900

    🚀 eslint+prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2021,
+    parser: "@typescript-eslint/parser",
+    ecmaFeatures: {
+      globalReturn: false,
+      impliedStrict: false,
+      jsx: false,
+    },
+    vueFeatures: {
+      filter: true,
+      interpolationAsNonHTML: false,
+      styleCSSVariableInjection: true,
+    },
+  },
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/strongly-recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/standard",
+    "@vue/prettier/@typescript-eslint",
+  ],
+  plugins: ["vue", "prettier"],
+  rules: {
+    "no-console":
+      process.env.NODE_ENV === "production"
+        ? ["error", { allow: ["warn", "error"] }]
+        : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "comma-dangle": "off",
+    "no-cond-assign": ["error", "always"],
+    "space-before-function-paren": "off",
+    indent: "off",
+    "prettier/prettier": "off",
+    "lines-between-class-members": "off",
+  },
+};
